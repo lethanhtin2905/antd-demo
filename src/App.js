e523d54b734190a68270cd729f454eb2370c5363
lethanhtin2905 <57804580+lethanhtin2905@users.noreply.github.com>
2021-12-11 23:18:12 +0700

    ver1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import { Button, Input, Space } from 'antd';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <br/>
+    <Space direction='vertical' size={10}>
+    <Input placeholder="Input text here" />
+    <br/>
+    <Button type="primary">Button</Button>
+    </Space>
+  </div>
   );
 }
 
---
+++ src/components/DatePicker.jsx
@@ -0,0 +1,9 @@
+//DatePicker.jsx
+import { Dayjs } from 'dayjs';
+import dayjsGenerateConfig from 'rc-picker/lib/generate/dayjs';
+import generatePicker from 'antd/es/date-picker/generatePicker';
+import 'antd/es/date-picker/style/index';
+
+const DatePicker = generatePicker<Dayjs>(dayjsGenerateConfig);
+
+export default DatePicker;
