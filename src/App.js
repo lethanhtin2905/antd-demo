184a95d6b07464717d8fa2a7385dfd57bac10048
lethanhtin2905 <57804580+lethanhtin2905@users.noreply.github.com>
2021-12-12 22:27:38 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import { Button, Input, Space } from 'antd';
-import './App.css';
+//App.js
+import React from 'react';
+import { Button, Space, Progress, Input } from 'antd';
+// import './App.css';
+import './App.less';
 
-function App() {
-  return (
-    <div className="App">
-    <br/>
-    <Space direction='vertical' size={10}>
-    <Input placeholder="Input text here" />
-    <br/>
+
+const App = () => (
+  <div className="App">
+    <Space direction='vertical' size={30}>
     <Button type="primary">Button</Button>
+    <Input placeholder='type here'></Input>
+    <Progress
+    strokeColor={{
+      '0%': '#108ee9',
+      '100%': '#87d068',
+    }}
+     percent={50} type='circle'></Progress>
+    <Button type="primary" danger>Danger Button</Button>
     </Space>
   </div>
-  );
-}
+);
 
 export default App;
